b7ea6744639bb87e71bf6af3a48d7cc166e98665
Martin Uche <martinuche19@outlook.com>
2024-06-28 02:11:01 -0500

    added routes and created pages

--- auth-ui/src/App.jsx
+++ auth-ui/src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Register from "./components/Register";
 
 export default function App() {
   return (
-    <h1 className="text-red-500">App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
