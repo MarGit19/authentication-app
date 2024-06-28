2e59c3dfc3cae276fe3cdafd0392f6c0b91684ee
Martin Uche <martinuche19@outlook.com>
2024-06-28 13:36:33 -0500

    created registration api route

--- api/index.js
+++ api/index.js
@@ -3,21 +3,22 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+//import cookieParser from 'cookie-parser';  // Import cookie-parser
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connected to MongoDB')
-}).catch((err) => {
-    console.log(err);
-});
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB', err);
+    });
 
 const app = express();
 
 app.use(express.json());
-
-app.listen(3000, () => {
-    console.log('Server listening on port 3000!')
-})
+//app.use(cookieParser());  // Use cookie-parser middleware
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -30,4 +31,9 @@ app.use((err, req, res, next) => {
         message,
         statusCode,
     });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
