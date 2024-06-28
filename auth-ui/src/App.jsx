d321a840d3c42883a746220c7483b7414e89e4a6
Martin Uche <martinuche19@outlook.com>
2024-06-28 02:27:40 -0500

    created header components

--- auth-ui/src/App.jsx
+++ auth-ui/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Login from "./components/Login";
-import Profile from "./components/Profile";
-import Register from "./components/Register";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import Header from "./components/Header";  // Ensure this import if Header is used
 
 export default function App() {
   return (
     <BrowserRouter>
+      {/* Header component */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
